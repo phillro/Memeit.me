6bfe8c106b3d7ac5b1fc080262e6a8264ae0725a
Phillip Rosen <phill@skyfetch.com>
2012-02-26 11:22:58 -0500

    adding partials

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,18 @@
-
 /*
  * GET home page.
  */
 
-exports.index = function(req, res){
-    models.streams.find({},function(err,results){
-        if(!results)
-            results=[]
-        res.render('index',{title:'Trollme.me',streams:results})
+exports.index = function (req, res) {
+    models.streams.find({}, function (err, results) {
+        if (req.query.ajax) {
+            if (!results)
+                results = []
+            res.partial('streamListPartial.ejs', {title:'Trollme.me', streams:results})
+        } else {
+            if (!results)
+                results = []
+            res.render('index', {title:'Trollme.me', streams:results})
+        }
     })
 
 };
